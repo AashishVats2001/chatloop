d7a4157ffad8b9ce3762fb522b459640d337ee37
AashishVats2001 <aashishvats.dev@gmail.com>
2025-07-04 12:48:37 +0530

    minor changes

--- components/AboutOptions.jsx
+++ components/AboutOptions.jsx
@@ -5,17 +5,17 @@ const AboutOptions = () => {
   return (
     <div className="w-full flex flex-col md:flex-row items-center pb-10 gap-5 justify-center border-b border-border-light text-text-gray text-lg md:text-xl lg:text-2xl font-medium">
       
-      <div className="w-full flex flex-col justify-center items-center gap-4 px-8 border-r border-border-light">
+      <div className="w-full flex flex-col justify-center items-center gap-4 px-8 md:border-r border-border-light">
         <Home size={48} />
         <p>Create Space</p>
       </div>
 
-      <div className="w-full flex flex-col justify-center items-center gap-4 px-8 border-r border-border-light">
+      <div className="w-full flex flex-col justify-center items-center gap-4 px-8 md:border-r border-border-light">
         <Users size={48} />
         <p>Invite People</p>
       </div>
 
-      <div className="w-full flex flex-col justify-center items-center gap-4 px-8 border-r border-border-light">
+      <div className="w-full flex flex-col justify-center items-center gap-4 px-8 md:border-r border-border-light">
         <Smile size={48} />
         <p>Hangout</p>
       </div>
